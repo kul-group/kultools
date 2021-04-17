b4ce90edfa580409d1b866e67c4bbe0754dfaeec
Jiawei Guo <jwguo@ucdavis.edu>
2021-04-16 17:20:05 -0700

    Update 00_make_molecule.py

--- 00_make_molecule.py
+++ 00_make_molecule.py
@@ -4,14 +4,14 @@
 import numpy as np  
 from ase.build import molecule
 from ase.visualize import view 
-from ase import view
+import ase.io as io
 
 
 atoms = molecule('H2O')
 atoms.set_cell(8*np.identity(3))
 atoms.center()
 view(atoms)
-io.write('water.traj')
+io.write('water.traj', atoms)
 
 
 

f0e50120aa000e8078964e9fa21439959a02a955
Jiawei <jwguo@ucdavis.edu>
2021-04-21 13:13:00 -0700

    add a new line

--- 00_make_molecule.py
+++ 00_make_molecule.py
@@ -6,9 +6,10 @@ from ase.build import molecule
 from ase.visualize import view 
 import ase.io as io
 
-
 atoms = molecule('H2O')
 atoms.set_cell(8*np.identity(3))
 atoms.center()
 view(atoms)
 io.write('start.traj', atoms)
+
+# here's a new add
